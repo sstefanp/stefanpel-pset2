26cc1050225fcd8f1ca0f926eb234c70691a5096
Stefan Pel <Stefan@wcw-stud-145-109-24-225.wireless.uva.nl>
2017-02-17 15:04:08 +0100

    Initial commit

---
+++ Mad Libs/WordViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WordViewController.swift
+//  Mad Libs
+//
+//  Created by Stefan Pel on 17-02-17.
+//  Copyright © 2017 Stefan Pel. All rights reserved.
+//
+
+import UIKit
+
+class WordViewController: UIViewController {
+    
+    // Variables
+        // stories to be chosen randomly
+        // placeholder for textField
+    
+    
+    // Outlets
+    @IBOutlet weak var textField: UITextField?
+    @IBOutlet weak var wordsLeftField: UILabel!
+    
+    // Actions
+    @IBAction func nextWordButton(_ sender: UIButton) {
+        //
+    }
+}
