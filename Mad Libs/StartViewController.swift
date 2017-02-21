ab4f5c90bec5d20714b42f41f3385bf744e4eb65
Stefan Pel <Stefan@wcw-stud-145-109-62-22.wireless.uva.nl>
2017-02-21 14:55:36 +0100

    Code finished

--- Mad Libs/StartViewController.swift
+++ Mad Libs/StartViewController.swift
@@ -8,7 +8,43 @@
 
 import UIKit
 
+
+
 class StartViewController: UIViewController {
+    
+    
+    @IBOutlet weak var lowerView: UIView!
+    @IBOutlet weak var logoView: UIImageView!
+    
+    
+    override func viewWillLayoutSubviews() {
+        // app is in landscape view
+        if self.view.bounds.width > self.view.bounds.height {
+            // text + button
+            self.lowerView.frame.size.width = 0.5 * self.view.bounds.width
+            self.lowerView.frame.origin.x = 0.5 * self.view.bounds.width
+            self.lowerView.frame.origin.y = 64
+            self.lowerView.frame.size.height = self.view.bounds.height
+            // Logo
+            self.logoView.frame.origin.x = 0
+            self.logoView.frame.origin.y = 64
+            self.logoView.frame.size.width = 0.5 * self.view.bounds.width
+            self.logoView.frame.size.height = self.view.bounds.height - 64
+        }
+            // app is in portret view
+        else {
+            // text + button
+            self.lowerView.frame.size.height = 0.5 * self.view.bounds.height
+            self.lowerView.frame.origin.x = 0
+            self.lowerView.frame.origin.y = 0.5 * self.view.bounds.height
+            self.lowerView.frame.size.width = self.view.bounds.width
+            // logo
+            self.logoView.frame.origin.x = 0
+            self.logoView.frame.origin.y = 44
+            self.logoView.frame.size.height = 0.5 * self.view.bounds.height - 44
+            self.logoView.frame.size.width = self.view.bounds.width
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Mad Libs/WordViewController.swift
+++ Mad Libs/WordViewController.swift
@@ -12,7 +12,7 @@ import Foundation
 class WordViewController: UIViewController {
     
     // Story
-    var story: Story!
+    //var story: Story!
     var text: String?
     
     // variables
@@ -28,38 +28,37 @@ class WordViewController: UIViewController {
     @IBOutlet weak var wordsLeftField: UILabel!
     @IBOutlet weak var wordsInputField: UITextField!
     @IBOutlet weak var nextWordButton: UIButton!
-    
-    // changing label to "all the words are filled in"
     @IBOutlet weak var upperLabel: UILabel!
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // hide to story button untill all words have been filled in
+        // hide to-story-button untill all words have been filled in
         newStoryButton.isHidden = true
         
-        // hide continue button unless word is filled in
-        //nextWordButton.isHidden = true
-        
-        // get new story
+        // randomly choose new story
         let array = ["/madlib0_simple", "/madlib1_tarzan", "/madlib2_university", "/madlib3_clothes", "/madlib4_dance"]
         let random = Int(arc4random_uniform(UInt32(array.count)))
-        let contents = try! String(contentsOfFile: Bundle.main.path(forResource: "\(array[random])", ofType: "txt")!)
-        storyNew = Story(stream: contents)
+        guard let contents = try? String(contentsOfFile: Bundle.main.path(forResource: "\(array[random])", ofType: "txt")!) else {
+            return
+        }
         
         // set story
         storyNew = Story(stream: contents)
         print(contents)
         
-        // update fields
+        // update count field with first placeholder
         countPlaceholder = storyNew.getPlaceholderRemainingCount()
         print(countPlaceholder! as Int)
         wordsLeftField.text = "\(String(describing: countPlaceholder!)) word(s) left"
+        
+        // update text field with first placeholder
         nextPlaceholderType = storyNew.getNextPlaceholder()
         wordsInputField.placeholder = "Fill in a(n) \(nextPlaceholderType!)"
     }
     @IBAction func nextWordButton(_ sender: Any) {
+        // last word
         if countPlaceholder == 1 {
             newStoryButton.isHidden = false
             nextWordButton.isEnabled = false
@@ -72,6 +71,8 @@ class WordViewController: UIViewController {
             nextWordButton.isHidden = false
             
             if countPlaceholder! >= 1 {
+                
+                // update count
                 countPlaceholder = countPlaceholder! - 1
                 wordsLeftField.text = "\(String(describing: countPlaceholder!)) word(s) left"
                 
@@ -85,14 +86,15 @@ class WordViewController: UIViewController {
                 
                 // if all the words are filled in
                 if countPlaceholder == 0 {
-                    // disable textfield
+                    // disable textfield and change label
                     upperLabel.text = "Jay! All the words are filled in!"
                     wordsInputField.isHidden = true
                     nextWordButton.isHidden = true
                     wordsLeftField.isHidden = true
                 }
                 else {
-                  wordsInputField.placeholder = "Fill in a(n) \(nextPlaceholderType!)"
+                    // update placeholder
+                    wordsInputField.placeholder = "Fill in a(n) \(nextPlaceholderType!)"
                 }
             }
         }
@@ -101,6 +103,7 @@ class WordViewController: UIViewController {
         text = storyNew.toString()
         print(text!)
     }
+    // sending text to TextViewController
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let textVC = segue.destination as? TextViewController {
             textVC.text = text
